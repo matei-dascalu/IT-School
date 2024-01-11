33f35cac97ce8486b2fc173b2c267a85b0bbca19
Matei Dascalu <dascalumatei@gmail.com>
2024-01-11 23:26:36 +0200

    updated functions

--- js-functions/script.js
+++ js-functions/script.js
@@ -1,6 +1,47 @@
+// const user = {
+//     username: "daniel",
+//     subscription: "MEDIUM",
+// };
+
+// const pages = [
+//     { name: "Homepage", subscriptions: ["LOW", "MEDIUM", "HIGH"] },
+//     { name: "Artists", subscriptions: ["MEDIUM", "HIGH"] },
+//     { name: "Lounge", subscriptions: ["HIGH"] },
+//     { name: "Listen", subscriptions: ["LOW", "MEDIUM"] },
+// ];
+
+// for (let page of pages) {
+//     if (page.name === "Lounge") {
+//         // if (page.subscriptions.includes(user.subscription)) {
+//         //     console.log("Utilizatorul are acces!");
+//         // } else {
+//         //     console.log("Utilizatorul NU are acces!");
+//         // }
+//         let userHasAccess = false;
+
+//         for (let subscription of page.subscriptions) {
+//             if (subscription === user.subscription) {
+//                 userHasAccess = true;
+//             }
+//         }
+
+//         if (userHasAccess) {
+//             console.log("Utilizatorul are acces!");
+//         } else {
+//             console.log("Utilizatorul NU are acces!");
+//         }
+//     }
+// }
+
+// 1. Avem un utilizator, care are un username si o subscriptie. Subscriptia poate fi LOW, MEDIUM, HIGH.
+
+// 2. Avem o lista de pagini, iar fiecare pagina contine numele si o lista de subscriptii care pot accesa pagina.
+
+// Sa se scrie o secventa de cod, care verifica daca utilizatorul de la punctul 1, are acces pe baza subscriptiei, la una din paginile de la punctul 2. Pagina este data.
+
 const user = {
     username: "daniel",
-    subscription: "MEDIUM",
+    subscription: "FREE TRIAL",
 };
 
 const user2 = {
@@ -8,27 +49,34 @@ const user2 = {
     subscription: "HIGH",
 };
 
+// Sa se scrie sau modifice o secventa de cod pentru a verifica daca user2 are acces la pagina "Lounge".
 
 const pages = [
-    { name: "Homepage", subscription: ["LOW", "MEDIUM", "HIGH"] },
-    { name: "Artists", subscription: ["MEDIUM", "HIGH"] },
-    { name: "Lounge", subscription: ["HIGH"] },
-    { name: "Listen", subscription: ["LOW", "MEDIUM"] },
+    { name: "Homepage", subscriptions: ["LOW", "MEDIUM", "HIGH"] },
+    { name: "Artists", subscriptions: ["MEDIUM", "HIGH"] },
+    { name: "Lounge", subscriptions: ["HIGH"] },
+    { name: "Listen", subscriptions: ["LOW", "MEDIUM"] },
 ];
+
+// Daca utilizatorul are FREE TRIAL, primeste acces automat, fara a se verifica subscriptiile necesare pentru accesarea paginii
 
 for (let page of pages) {
     if (page.name === "Listen") {
         let userHasAccess = false;
 
-        for (let acceptedSubscription of page.subscription) {
-            if (acceptedSubscription === user.subscription) {
-                userHasAccess = true;
-                break;
+        if (user.subscription === "FREE TRIAL") {
+            userHasAccess = true;
+        } else {
+            for (let acceptedSubscription of page.subscriptions) {
+                if (acceptedSubscription === user.subscription) {
+                    userHasAccess = true;
+                    break;
+                }
             }
         }
 
         if (userHasAccess) {
-            console.log("are acces");
+            console.log("Are acces");
         } else {
             console.log("NU are acces");
         }
@@ -37,23 +85,9 @@ for (let page of pages) {
     }
 }
 
-for (let page of pages) {
-    if (page.name === "Lounge") {
-        let userHasAccess = false;
-
-        for (let acceptedSubscription of page.subscription) {
-            if (acceptedSubscription === user2.subscription) {
-                userHasAccess = true;
-                break;
-            }
-        }
-
-        if (userHasAccess) {
-            console.log("are acces");
-        } else {
-            console.log("NU are acces");
-        }
-
-        break;
-    }
-}
+/**
+ * 1. Trece prin fiecare pagina
+ * 2. Verifica daca numele paginii corespunde cu cea cautata
+ * 3. Verifica daca utilizatorul are o subscriptie valida pentru accesarea paginii.
+ * 4. Asigura sau respinge accesul utilizatorului.
+ */
